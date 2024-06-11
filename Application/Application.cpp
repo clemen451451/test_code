2107702225f19613502c9a0e4c97fb223618f56f
Clement Hanson <hanson.clement@yandex.ru>
2024-06-11 04:50:22 +0300

    Update

---
+++ Application/Application.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    std::string string = "skillbox";
+
+    std::cout << "string: " << string << "\n";
+    std::cout << "length: " << string.length() << "\n";
+    std::cout << string[0] << " - " << string[string.length() - 1];
+}
+
