aa295458c56592b8f01358e43fd675313a22ccf6
Clement Hanson <hanson.clement@yandex.ru>
2024-06-25 11:06:14 +0300

    update

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -1,35 +1,41 @@
 ﻿
 #include <iostream>
-
-void PrintNumbersInConsole(int MaxNumber, bool IsOdd);
-
-const int MaxNumber = 9;
+#include <time.h>
 
 int main()
 {
-    PrintNumbersInConsole(MaxNumber, false);
-    PrintNumbersInConsole(MaxNumber, true);
+	const int arraySize = 8;
+
+	int array[arraySize][arraySize] = {};
+
+	for (int i = 0; i < arraySize; i++)
+	{
+		for (int j = 0; j < arraySize; j++)
+			array[i][j] = i + j;
+	}
+
+	for (int i = 0; i < arraySize; i++)
+	{
+		for (int j = 0; j < arraySize; j++)
+			std::cout << array[i][j];
+
+		std::cout << "\n";
+	}
+
+	std::cout << "\n\n";
+
+	struct tm buf;
+	time_t t = time(NULL);
+	
+	localtime_s(&buf, &t);
+
+	int index = buf.tm_mday % 2;
+	int sum = 0;
+
+	for (int j = 0; j < arraySize; j++)
+		sum += array[index][j];
+
+	std::cout << "Summ " << index << " string: " << sum;
+
 }
 
-void PrintNumbersInConsole(int MaxNumber, bool IsOdd)
-{
-    std::cout << "\n\n";
-
-    for (int i = 0; i < MaxNumber; i++)
-    {
-        if (!IsOdd)
-        {
-            if (!(i % 2))
-            {
-                std::cout << i << "\n";
-            }
-        }
-        else
-        {
-            if (i % 2)
-            {
-                std::cout << i << "\n";
-            }
-        }
-    }
-}
