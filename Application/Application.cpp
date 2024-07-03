ad50b8c69d80f66b0b1d01885408152129546f1a
Clement Hanson <hanson.clement@yandex.ru>
2024-07-03 13:05:21 +0300

    update

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -1,36 +1,103 @@
 ﻿
 #include <iostream>
-#include <cmath>
 
-class Vector
+class Player
 {
-private:
-	int x;
-	int y;
+	private:
+		std::string name;
+		int scores;
 
-public:
-	Vector() : x(0), y(0)
-	{}
+	public:
+		Player() : name("None"), scores(0)
+		{}
 
-	Vector(int _x, int _y) : x(_x), y(_y)
-	{}
+		Player(std::string _name, int _scores) : name(_name), scores(_scores)
+		{}
 
-	double GetVectorSize()
-	{
-		return sqrt((x * x) + (y * y));
-	}
+	public:
+		std::string Name() const { return name; };
+		int Scores() const { return scores; };
 
-	void ShowVectorValues()
-	{
-		std::cout << "x: " << x << " y: " << y;
-	}
+	public:
+
+		void SetName(std::string _name)
+		{
+			name = _name;
+		}
+
+		void SetScores(int _scores)
+		{
+			scores = _scores;
+		}
+};
+
+class Administrator
+{
+	public:
+
+		void InsertPlayers(Player* players, int playersAmount)
+		{
+			for (int i = 0; i < playersAmount; i++)
+			{
+				std::string name;
+				int scores;
+
+				std::cout << "Enter player name: " << "\n";
+				std::cin >> name;
+
+				players[i].SetName(name);
+
+				std::cout << "Enter player scores: " << "\n";
+				std::cin >> scores;
+
+				players[i].SetScores(scores);
+			}
+		}
+
+		void SortingPlayers(Player* players, int playersAmount)
+		{
+			for (int i = 0; i < playersAmount; i++)
+			{
+				for (int j = i; j < playersAmount; j++)
+				{
+					if (players[i].Scores() < players[j].Scores())
+					{
+						std::string tempName = players[i].Name();
+						int tempScores = players[i].Scores();
+
+						players[i].SetScores(players[j].Scores());
+						players[j].SetScores(tempScores);
+
+						players[i].SetName(players[j].Name());
+						players[j].SetName(tempName);
+					}
+				}
+			}
+		}
+
+		void GetPlayersList(Player* players, int playersAmount)
+		{
+			for (int i = 0; i < playersAmount; i++)
+			{
+				std::cout << "PlayerInfo: " << players[i].Name() << " (scores: " << players[i].Scores() << ")" << "\n";
+			}
+		}
 };
 
 int main()
 {
-	Vector vector(3, 5);
+	Administrator administrator;
+	int playersAmount;
 
-	vector.ShowVectorValues();
+	std::cout << "Enter ammount players:";
+	std::cin >> playersAmount;
 
-	std::cout << "\n" << "Size: " << vector.GetVectorSize();
+	Player* players { new Player[playersAmount]() };
+
+	administrator.InsertPlayers(players, playersAmount);
+	administrator.SortingPlayers(players, playersAmount);
+	administrator.GetPlayersList(players, playersAmount);
+
+	delete[] players;
 }
+
