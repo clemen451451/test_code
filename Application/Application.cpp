99785748b32202cf15e1a062519402a85733b747
Clement Hanson <hanson.clement@yandex.ru>
2024-07-09 04:30:45 +0300

    update

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -1,103 +1,50 @@
 ﻿
 #include <iostream>
 
-class Player
+class Animal
 {
-	private:
-		std::string name;
-		int scores;
-
-	public:
-		Player() : name("None"), scores(0)
-		{}
-
-		Player(std::string _name, int _scores) : name(_name), scores(_scores)
-		{}
-
-	public:
-		std::string Name() const { return name; };
-		int Scores() const { return scores; };
-
-	public:
-
-		void SetName(std::string _name)
-		{
-			name = _name;
-		}
-
-		void SetScores(int _scores)
-		{
-			scores = _scores;
-		}
+public:
+	virtual void Voice()
+	{
+		std::cout << "Text Animal\n";
+	}
 };
 
-class Administrator
+class Dog : public Animal
 {
-	public:
+public: 
+	void Voice() override
+	{
+		std::cout << "Woof!\n";
+	}
+};
 
-		void InsertPlayers(Player* players, int playersAmount)
-		{
-			for (int i = 0; i < playersAmount; i++)
-			{
-				std::string name;
-				int scores;
+class Cat : public Animal
+{
+public:
+	void Voice() override
+	{
+		std::cout << "Meow!\n";
+	}
+};
 
-				std::cout << "Enter player name: " << "\n";
-				std::cin >> name;
-
-				players[i].SetName(name);
-
-				std::cout << "Enter player scores: " << "\n";
-				std::cin >> scores;
-
-				players[i].SetScores(scores);
-			}
-		}
-
-		void SortingPlayers(Player* players, int playersAmount)
-		{
-			for (int i = 0; i < playersAmount; i++)
-			{
-				for (int j = i; j < playersAmount; j++)
-				{
-					if (players[i].Scores() < players[j].Scores())
-					{
-						std::string tempName = players[i].Name();
-						int tempScores = players[i].Scores();
-
-						players[i].SetScores(players[j].Scores());
-						players[j].SetScores(tempScores);
-
-						players[i].SetName(players[j].Name());
-						players[j].SetName(tempName);
-					}
-				}
-			}
-		}
-
-		void GetPlayersList(Player* players, int playersAmount)
-		{
-			for (int i = 0; i < playersAmount; i++)
-			{
-				std::cout << "PlayerInfo: " << players[i].Name() << " (scores: " << players[i].Scores() << ")" << "\n";
-			}
-		}
+class Cow : public Animal
+{
+public:
+	void Voice() override
+	{
+		std::cout << "Moo!\n";
+	}
 };
 
 int main()
 {
-	Administrator administrator;
-	int playersAmount;
+	const int sizeArray = 3;
+	Animal* ptrArray[sizeArray] = { { new Dog() }, { new Cat() }, { new Cow() } };
 
-	std::cout << "Enter ammount players:";
-	std::cin >> playersAmount;
-
-	Player* players { new Player[playersAmount]() };
-
-	administrator.InsertPlayers(players, playersAmount);
-	administrator.SortingPlayers(players, playersAmount);
-	administrator.GetPlayersList(players, playersAmount);
-
-	delete[] players;
+	for (int i = 0; i < sizeArray; i++)
+	{
+		ptrArray[i]->Voice();
+	}
 }
 
